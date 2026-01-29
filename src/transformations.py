a4e5a9cd2443c5e582db9eafea9aa427e8c89d00
Jakub Milczarczyk <milczarczykjakub@gmail.com>
2026-01-29 11:16:18 +0100

    feat(silver): implement robust transformation logic, unit tests and CI fix
    - Implemented standardize_security_logs with defensive column selection for schema drift.
    - Added Unit Tests using pytest and local SparkSession.
    - Fixed CI/CD environment issues (Python path resolution on Windows).
    - Refactored project structure.

--- src/transformations.py
+++ src/transformations.py
@@ -1,4 +1,5 @@
-"""Siver layer transformations.
+"""
+Silver layer transformations.
 Standarizes raw security logs into a schema optimized for Threat Hunting.
 """
 
@@ -15,47 +16,104 @@ def standarize_security_logs(df: DataFrame) -> DataFrame:
         df (DataFrame): Raw Bronze DataFrame. 
 
     Returns:
-        DataFrame: Standarized Silver DataFrame with 'event_timestamp' and 'host'.
+        DataFrame: Standarized Silver DataFrame.
     """
+    # Helper Function: Safe Column Select
+    def safe_col(col_name: str):
+        if col_name in df.columns:
+            return F.col(col_name)
+        return F.lit(None)
 
+    # Timestamp Standarization
     timestamp_col = F.coalesce(
-        F.col("@timestamp"),
-        F.col("EventTime")
+        safe_col("@timestamp"),
+        safe_col("EventTime")
     ).cast(TimestampType())
 
+    # Hostname Standarization
     host_col = F.coalesce(
-        F.col("Hostname"),
-        F.col("host"),
-        F.col("Computer")
+        safe_col("Hostname"),
+        safe_col("host"),
+        safe_col("Computer")
     )
 
-    def secure_col(col_name: str, nested_fallback: str = None): 
-        if nested_fallback:
-            return F.coalesce(F.col(col_name), F.col(nested_fallback))
-        return F.col(col_name)
+    # User Standarization
+    user_col = F.coalesce(
+        safe_col("AccountName"),
+        safe_col("User"),
+        safe_col("UserID")
+    )
 
+    # Nasted Fields Handling
+    def secure_nasted(flat_name, nasted_name):
+        cols_to_check = []
+
+        if flat_name in df.columns:
+            cols_to_check.append(F.col(flat_name))
+
+        if "EventData" in df.columns:
+            cols_to_check.append(F.col(nasted_name))
+
+        if not cols_to_check:
+            return F.lit(None).cast("string")
+        
+        return F.coalesce(*cols_to_check)
+    
+    # Define Transformations
     df_silver = df.select(
         timestamp_col.alias("event_timestamp"),
-        F.col("EventID").cast(IntegerType()).alias("event_id"),
-        F.col("Channel").alias("log_channel"),
+    
+        # Event ID
+        safe_col("EventID").cast(IntegerType()).alias("event_id"),
+
+        safe_col("Channel").alias("log_channel"),
         host_col.alias("hostname"),
+        user_col.alias("user_account"),
 
-        # User Context
-        F.coalesce(F.col("AccountName"), F.col("User"), F.col("UserID")).alias("user_account"),
-
-        # Process / Execution Context
-        secure_col("SourceImage", "EventData.SourceImage").alias("process_image"),
-        secure_col("TargetImage", "EventData.TargetImage").alias("target_process_image"),
-        secure_col("CommandLine", "EventData.CommandLine").alias("command_line"),
+        # Process / Ececution Context
+        secure_nasted("SourceImage", "EventData.SourceImage").alias("process_image"),
+        secure_nasted("TargetImage", "EventData.TargetImage").alias("target_process_image"),
+        secure_nasted("CommandLine", "EventData.CommandLine").alias("command_line"),
 
         # Network Context
-        secure_col("SourceIp", "EventData.SourceIp").alias("source_ip"),
-        secure_col("DestinationIp", "EventData.DestinationIp").alias("dest_ip"),
-        secure_col("DestinationPort", "EventData.DestinationPort").alias("dest_port"),
+        secure_nasted("SourceIp", "EventData.SourceIp").alias("source_ip"),
+        secure_nasted("DestinationIp", "EventData.DestinationIp").alias("dest_ip"),
+        secure_nasted("DestinationPort", "EventData.DestinationPort").alias("dest_port"),
 
         # Threat Context
-        F.col("tags").alias("detection_tags")
+        safe_col("tags").alias("detetion_tags")
     )
 
     # Data Quality Checks
-    return df_silver.filter(F.col(event_timestamp).isNotNull())
+    return df_silver.filter(F.col("event_timestamp").isNotNull())
+
+    # def secure_col(col_name: str, nested_fallback: str = None): 
+    #     if nested_fallback:
+    #         return F.coalesce(F.col(col_name), F.col(nested_fallback))
+    #     return F.col(col_name)
+
+    # df_silver = df.select(
+    #     timestamp_col.alias("event_timestamp"),
+    #     F.col("EventID").cast(IntegerType()).alias("event_id"),
+    #     F.col("Channel").alias("log_channel"),
+    #     host_col.alias("hostname"),
+
+    #     # User Context
+    #     F.coalesce(F.col("AccountName"), F.col("User"), F.col("UserID")).alias("user_account"),
+
+    #     # Process / Execution Context
+    #     secure_col("SourceImage", "EventData.SourceImage").alias("process_image"),
+    #     secure_col("TargetImage", "EventData.TargetImage").alias("target_process_image"),
+    #     secure_col("CommandLine", "EventData.CommandLine").alias("command_line"),
+
+    #     # Network Context
+    #     secure_col("SourceIp", "EventData.SourceIp").alias("source_ip"),
+    #     secure_col("DestinationIp", "EventData.DestinationIp").alias("dest_ip"),
+    #     secure_col("DestinationPort", "EventData.DestinationPort").alias("dest_port"),
+
+    #     # Threat Context
+    #     F.col("tags").alias("detection_tags")
+    # )
+
+    # # Data Quality Checks
+    # return df_silver.filter(F.col(event_timestamp).isNotNull())
---
+++ tests/test_silver.py
@@ -0,0 +1,60 @@
+"""
+Test cases for silver layer.
+"""
+
+import pytest
+from src.transformations import standarize_security_logs
+from pyspark.sql.types import StructType, StructField, StringType, IntegerType, ArrayType
+
+def test_standarize_sysmon_event_10(spark):
+    """
+    Scenario: Sysmon Event 10 (Process Access) from Mordor Dataset.
+    Goal: Verify if flat JSON fields are correctly mapped to Silver schema.
+    """
+    # 1. Arrange
+    data = [{
+        "@timestamp": "2020-09-20T06:57:17.371Z",
+        "EventID": 10,
+        "Hostname": "WORKSTATION5.theshire.local",
+        "Channel": "Microsoft-Windows-Sysmon/Operational",
+        "SourceImage": "C:\\windows\\system32\\svchost.exe",
+        "TargetImage": "C:\\windows\\System32\\svchost.exe",
+        "AccountName": "SYSTEM",
+        "tags": ["mordorDataset"]
+    }]
+
+    df_raw = spark.createDataFrame(data)
+
+    # 2. Act
+    df_silver = standarize_security_logs(df_raw)
+
+    # 3. Assert
+    result = df_silver.collect()[0]
+
+    assert result["event_id"] == 10
+    assert result["hostname"] == "WORKSTATION5.theshire.local"
+
+    assert result["process_image"] == "C:\\windows\\system32\\svchost.exe"
+
+    assert result["event_timestamp"].year == 2020
+
+def test_standarize_powershell_missing_fields(spark):
+    """
+    Scenario: PowerShell Event 4103.
+    Goal: Verify that missing fields (e.g., SourceImage) result in Nulls, not Errors.
+    """
+    data = [{
+        "@timestamp": "2020-09-20T06:57:17.372Z",
+        "EventID": 4103,
+        "Channel": "Microsoft-Windows-PowerShell/Operational",
+        "ContextInfo": "Severity = Informational..."
+        # No SourceImage in this record
+    }]
+
+    df_raw = spark.createDataFrame(data)
+    df_silver = standarize_security_logs(df_raw)
+    result = df_silver.collect()[0]
+
+    assert result["event_id"] == 4103
+    assert result["process_image"] is None  # SourceImage was missing
+
