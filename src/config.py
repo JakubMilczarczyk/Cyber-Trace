9879cf0531ef2e0010b539dbae2a1d5c328f9c6b
JakubMilczarczyk <milczarczykjakub@gmail.com>
2026-02-03 15:56:52 +0000

    Dev: Working Silver Layer
    - Bronze and Silver Layers are work correctly.
    - All variables are set in scr/config.py file.
    - All transforming functions create in scr/transformations.py
    - Notebooks are only executable files.

---
+++ src/config.py
@@ -0,0 +1,32 @@
+class ProjectConfig:
+    STORAGE_ACCOUNT = "cybertracebronze"
+    CONTAINER_NAME = "bronze"
+    SCOPE_NAME = "cybertrace-secrets"
+    KEY_NAME = "storage-access-key"
+
+    @staticmethod
+    def get_base_path():
+        return f"abfss://{ProjectConfig.CONTAINER_NAME}@{ProjectConfig.STORAGE_ACCOUNT}.dfs.core.windows.net"
+
+
+class Paths:
+    BASE_PATH = ProjectConfig.get_base_path()
+    RAW_LOGS = f"{BASE_PATH}/raw_logs"
+    SCHEMA = f"{BASE_PATH}/schemas/ingestion_schema"
+    CHECKPOINT_BRONZE = f"{BASE_PATH}/checkpoints/bronze_mordor_logs"
+    CHECKPOINT_SILVER = f"{BASE_PATH}/checkpoints/silver_mordor_logs"
+    QUARANTINE = f"{BASE_PATH}/_quarantine"
+
+
+
+def setup_authentication(spark, dbutils):
+    """Authenticates Spark session to ADLS via Key Vault."""
+    try:
+        key = dbutils.secrets.get(scope=ProjectConfig.SCOPE_NAME, key=ProjectConfig.KEY_NAME)
+        spark.conf.set(
+            f"fs.azure.account.key.{ProjectConfig.STORAGE_ACCOUNT}.dfs.core.windows.net",
+            key
+        )
+        print("AUTH: Connected to ADLS Gen2")
+    except Exception as e:
+        raise RuntimeError(f"AUTH ERROR: Failed to connect to ADLS Gen2: {e}")
--- src/transformations.py
+++ src/transformations.py
@@ -7,16 +7,16 @@ from pyspark.sql import DataFrame
 from pyspark.sql import functions as F
 from pyspark.sql.types import TimestampType, IntegerType
 
-def standarize_security_logs(df: DataFrame) -> DataFrame:
+def standardize_security_logs(df: DataFrame) -> DataFrame:
     """
-    Standarizes schema for mixed security logs (Sysmon, PowerShell).
+    Standardizes schema for mixed security logs (Sysmon, PowerShell).
     Handles both flat and nested JSON structures via coalescing.
 
     Args:
         df (DataFrame): Raw Bronze DataFrame. 
 
     Returns:
-        DataFrame: Standarized Silver DataFrame.
+        DataFrame: Standardized Silver DataFrame.
     """
     # Helper Function: Safe Column Select
     def safe_col(col_name: str):
@@ -24,20 +24,20 @@ def standarize_security_logs(df: DataFrame) -> DataFrame:
             return F.col(col_name)
         return F.lit(None)
 
-    # Timestamp Standarization
+    # Timestamp Standardization
     timestamp_col = F.coalesce(
         safe_col("@timestamp"),
         safe_col("EventTime")
     ).cast(TimestampType())
 
-    # Hostname Standarization
+    # Hostname Standardization
     host_col = F.coalesce(
         safe_col("Hostname"),
         safe_col("host"),
         safe_col("Computer")
     )
 
-    # User Standarization
+    # User Standardization
     user_col = F.coalesce(
         safe_col("AccountName"),
         safe_col("User"),
@@ -45,7 +45,7 @@ def standarize_security_logs(df: DataFrame) -> DataFrame:
     )
 
     # Nasted Fields Handling
-    def secure_nasted(flat_name, nasted_name):
+    def secure_nested(flat_name, nasted_name):
         cols_to_check = []
 
         if flat_name in df.columns:
@@ -71,49 +71,19 @@ def standarize_security_logs(df: DataFrame) -> DataFrame:
         user_col.alias("user_account"),
 
         # Process / Ececution Context
-        secure_nasted("SourceImage", "EventData.SourceImage").alias("process_image"),
-        secure_nasted("TargetImage", "EventData.TargetImage").alias("target_process_image"),
-        secure_nasted("CommandLine", "EventData.CommandLine").alias("command_line"),
+        secure_nested("SourceImage", "EventData.SourceImage").alias("process_image"),
+        secure_nested("TargetImage", "EventData.TargetImage").alias("target_process_image"),
+        secure_nested("CommandLine", "EventData.CommandLine").alias("command_line"),
 
         # Network Context
-        secure_nasted("SourceIp", "EventData.SourceIp").alias("source_ip"),
-        secure_nasted("DestinationIp", "EventData.DestinationIp").alias("dest_ip"),
-        secure_nasted("DestinationPort", "EventData.DestinationPort").alias("dest_port"),
+        secure_nested("SourceIp", "EventData.SourceIp").alias("source_ip"),
+        secure_nested("DestinationIp", "EventData.DestinationIp").alias("dest_ip"),
+        secure_nested("DestinationPort", "EventData.DestinationPort").alias("dest_port"),
 
         # Threat Context
-        safe_col("tags").alias("detetion_tags")
+        safe_col("tags").alias("detection_tags")
     )
 
     # Data Quality Checks
     return df_silver.filter(F.col("event_timestamp").isNotNull())
 
-    # def secure_col(col_name: str, nested_fallback: str = None): 
-    #     if nested_fallback:
-    #         return F.coalesce(F.col(col_name), F.col(nested_fallback))
-    #     return F.col(col_name)
-
-    # df_silver = df.select(
-    #     timestamp_col.alias("event_timestamp"),
-    #     F.col("EventID").cast(IntegerType()).alias("event_id"),
-    #     F.col("Channel").alias("log_channel"),
-    #     host_col.alias("hostname"),
-
-    #     # User Context
-    #     F.coalesce(F.col("AccountName"), F.col("User"), F.col("UserID")).alias("user_account"),
-
-    #     # Process / Execution Context
-    #     secure_col("SourceImage", "EventData.SourceImage").alias("process_image"),
-    #     secure_col("TargetImage", "EventData.TargetImage").alias("target_process_image"),
-    #     secure_col("CommandLine", "EventData.CommandLine").alias("command_line"),
-
-    #     # Network Context
-    #     secure_col("SourceIp", "EventData.SourceIp").alias("source_ip"),
-    #     secure_col("DestinationIp", "EventData.DestinationIp").alias("dest_ip"),
-    #     secure_col("DestinationPort", "EventData.DestinationPort").alias("dest_port"),
-
-    #     # Threat Context
-    #     F.col("tags").alias("detection_tags")
-    # )
-
-    # # Data Quality Checks
-    # return df_silver.filter(F.col(event_timestamp).isNotNull())
